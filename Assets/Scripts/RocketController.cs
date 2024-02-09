fcc18b60dda8dbafcdd30072ec519acf1b431c7b
Miroslav Maksimović <maksimovic.miroslav999@gmail.com>
2024-02-09 03:33:00 +0100

    Fixing PlayerRocket
    Rocket used to disappear from view after a while, because z axis was also changing with others making it go below camera reach

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -7,7 +7,7 @@ public class RocketController : MonoBehaviour
     public delegate void RocketDestroyedDelegate();
     public event RocketDestroyedDelegate OnRocketDestroyed;
 
-    private Vector3 _targetPosition;
+    private Vector2 _targetPosition;
 
     public void SetTarget(Vector3 target)
     {
@@ -21,27 +21,15 @@ public class RocketController : MonoBehaviour
 
     void MoveRocket()
     {
-        Vector3 direction = (_targetPosition - transform.position).normalized;
-
-        // Calculate the rotation angle to look towards the target
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // Set the rotation of the rocket to face towards the target
-        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
-
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, rocketSpeed * Time.deltaTime);
-
-       //Vector3 velocity = direction * rocketSpeed;
-       //float movementStep = rocketSpeed * Time.deltaTime;
-       //Debug.Log("MovementStep: " + movementStep);
-       
-       // Move the rocket towards the target with constant speed
-       //transform.position += velocity * Time.deltaTime;
-
+        Vector2 rocketPosition = transform.position;
+        Vector2 direction = _targetPosition - rocketPosition;
         
-        if (transform.position == _targetPosition)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+        transform.position = Vector3.MoveTowards(rocketPosition, _targetPosition, rocketSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _targetPosition) <= 0)
         {
-            //Vector3.Distance(transform.position, _targetPosition)
             //Debug.Log("Rocket position: " + transform.position);
             //Debug.Log("Target position: " + targetPosition);
             //Debug.Log("Move: " + rocketSpeed * Time.deltaTime);
@@ -54,7 +42,7 @@ public class RocketController : MonoBehaviour
         Destroy(gameObject);
         if (OnRocketDestroyed != null)
         {
-            //OnRocketDestroyed();
+            OnRocketDestroyed();
         }
     }
 }
